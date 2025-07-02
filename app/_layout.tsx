0425b884747a5a58d83943079a031df863b7ea84
wolf0dev <wolf.diagnostic@gmail.com>
2025-07-01 21:25:09 -0400

    Added useFrameworkReady.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,14 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from 'expo-splash-screen'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useFrameworkReady();
   const [fontsLoaded] = useFonts({
     // Add any custom fonts here if needed
   });
