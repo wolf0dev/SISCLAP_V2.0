a025f8abb70c3e8b8348a128039eee2bacd9dd53
wolf0dev <wolf.diagnostic@gmail.com>
2025-07-04 18:28:40 -0400

    Corregir errores de compilación de Android

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,12 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Ensure that all platforms are enabled
+config.transformer.minifierConfig = {
+  keep_fnames: true,
+  mangle: {
+    keep_fnames: true,
+  },
+};
+
 module.exports = config;
